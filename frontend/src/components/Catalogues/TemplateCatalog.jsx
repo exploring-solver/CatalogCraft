4ec3afe08a44d6830644c4068f85aac3268b07b5
exploring-solver <amansharma12607@gmail.com>
2024-05-19 19:59:30 +0530

    Deleted whole frontend web

--- frontend/src/components/Catalogues/TemplateCatalog.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const TemplateCatalog = () => {
-  return (
-    <div>TemplateCatalog</div>
-  )
-}
-
-export default TemplateCatalog
--- frontend/src/components/Profile/ProfileScreen.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ProfileScreen = () => {
-  return (
-    <div>ProfileScreen</div>
-  )
-}
-
-export default ProfileScreen
